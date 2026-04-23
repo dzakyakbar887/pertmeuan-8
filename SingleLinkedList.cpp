5036ce1ad22a0484b03aa6cfec0ca714b340d765
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-04-23 12:32:11 +0700

    mencari data dalam list

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -127,4 +127,5 @@ int main()
         cout << "1. Menambah data ke dalam list" << endl;
         cout << "2. Menghapus data dari dalam list" << endl;
         cout << "3. Menampilkan semua data didalam list" << endl;
-        
+        cout << "4. Mencari data dalam list" << endl;
+       
